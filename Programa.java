58d211218c2bdb9a2b1b07201784ccf7cfc348b6
Miguel Jaque Barbero <mjaque@migueljaque.com>
2025-12-03 20:26:18 +0100

    Primera versión

---
+++ Programa.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Programa{
+	static Scanner teclado = new Scanner(System.in);
+	static Envio[] envios = new Envio[50];
+	static Integer iEnvios = 0;
+
+	public static void main(String[] args){
+		Boolean repetirMenu = true;
+		do{
+			switch(verMenu()){
+				case "1":
+					registrarEnvio();
+					break;
+				case "2":
+					break;
+				case "X":
+				case "x":
+					repetirMenu = false;
+					break;
+				default:
+					System.out.println("Opción desconocida.");
+			}
+		}while(repetirMenu);
+		teclado.close();
+		System.out.println("Fin del programa. Espero volver a verle pronto.\n");
+	}
+
+	public static String verMenu(){
+		System.out.println("APLICACIÓN DE PaqueteMetes version 1.0");
+		System.out.println("\t1. Registrar Envío");
+		System.out.println("\t2. Ver Informe de Envíos");
+		System.out.println("\tX. Salir");
+		System.out.print("\tOpción: ");
+		return teclado.nextLine();
+	}
+
+	public static void registrarEnvio(){
+		while(true){
+			try{
+				System.out.println("\nRegistro de Envío");
+				System.out.print("\tNúmero: ");
+				String numero = teclado.nextLine();
+				System.out.print("\tPrecio: ");
+				Float precio = Float.valueOf(teclado.nextLine());
+
+				Envio envio = new Envio();
+				envio.numero = numero;
+				envio.precio = precio;
+				envios[iEnvios++] = envio;
+				
+				System.out.println("Registro OK");
+			}
+			catch(Exception excepcion){
+				System.out.println("Registro KO");
+			}
+		}
+	}
+
+}
